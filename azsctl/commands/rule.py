2ffff75e2985d7f1d3e51727920093706fca74f3
frodehus <frode.hus@outlook.com>
2021-01-01 22:25:32 +0100

    added rule selection ui

--- azsctl/commands/rule.py
+++ azsctl/commands/rule.py
@@ -1,3 +1,4 @@
+from PyInquirer.prompt import prompt
 from azsctl.api import AzureSentinelApi
 from azsctl.validators import ScheduledRuleValidator
 import yaml
@@ -20,7 +21,11 @@ def run_rule_query(rule_id: str):
     data = execute_query(query)
     return data
 
-def edit_rule(rule_id: str):
+
+def edit_rule(rule_id: str = None):
+    if not rule_id:
+        rule_id = select_rule()
+
     from subprocess import call
     import tempfile, os, json
 
@@ -28,17 +33,17 @@ def edit_rule(rule_id: str):
     EDITOR = os.getenv("EDITOR", "vim")
     rule = get_rule(rule_id)
     del rule["properties"]["lastModifiedUtc"]
-    
+
     with tempfile.NamedTemporaryFile(suffix=".rule", delete=False, mode="w") as temp:
         rule_to_edit = json.dumps(rule, indent=2)
         temp.write(rule_to_edit)
         temp.flush()
         call([EDITOR, temp.name], shell=False)
-    
+
     with open(temp.name, "r") as edited_rule:
         rule = json.load(edited_rule)
         result = api.update_alert_rule(rule_id, rule)
-    
+
     os.remove(temp.name)
     return result
 
@@ -57,3 +62,19 @@ def import_rule(file: str, validate_only: bool = False):
 def validate_rule(rule: dict):
     validator = ScheduledRuleValidator(rule)
     return validator.validate()
+
+
+def select_rule():
+    rules = list_rules()
+    select = {
+        "type": "list",
+        "name": "rule",
+        "message": "Which rule do you wish to edit?",
+        "choices": [rule["properties"]["displayName"] for rule in rules],
+    }
+
+    answer = prompt(select)
+    rule_id = (
+        list(filter(lambda x: x["properties"]["displayName"] == answer["rule"], rules))
+    )[0]
+    return rule_id["name"]
